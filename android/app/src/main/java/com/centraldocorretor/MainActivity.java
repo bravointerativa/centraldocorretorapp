4d63de5bda744e60104c295d324241defc09fd5b
Daniela <daniela@bravo.st>
2022-03-03 08:45:12 -0300

    1.0.1

---
+++ android/app/src/main/java/com/centraldocorretor/MainActivity.java
@@ -0,0 +1,15 @@
+package construtoradiamond.centraldocorretor;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "centraldocorretor";
+  }
+}
