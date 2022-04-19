893d226f19b5f45292a2ad3ece4a51a3867a55cc
Daniela <daniela@bravo.st>
2022-04-19 16:16:45 -0300

    ajustes imagens app

--- App.js
+++ App.js
@@ -1,39 +1,23 @@
 import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { StyleSheet, StatusBar, SafeAreaView } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 
-SplashScreen.preventAutoHideAsync() // <- previne a splashscreen de se esconder automaticamente 
-
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
   React.useEffect(() => {
     setTimeout(async () => {
       await SplashScreen.hideAsync();
-    }, 5000); // <-- Para a Splashscreen se esconder após 5segundos
+    }, 3000);
   }, []);
 
   return (
-    <>
-      <SafeAreaView style={styles.topo}>
-        <WebView
-          style={styles.container}
-          source={{
-            uri: 'https://centraldocorretor.construtoradiamond.com.br/?acesso_app',
-          }}
-        />
-      </SafeAreaView>
-    </>
-  )
+    <WebView
+      source={{ uri: 'https://centraldocorretor.construtoradiamond.com.br/?acesso_app' }}
+      startInLoadingState={true}
+      renderLoading={this.renderLoading}
+    />
+  );
 }
-
-const styles = StyleSheet.create({
-  topo: {
-    flex: 1,
-    backgroundColor: '#2f2f2f',
-  },
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-});
